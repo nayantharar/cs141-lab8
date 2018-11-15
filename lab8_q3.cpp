fcfce9b6b89dda85b30086e0dd5345d04d05fa3e
nayantharar <42238330+nayantharar@users.noreply.github.com>
2018-11-15 17:36:45 +0530

    Update lab8_q3.cpp

--- lab8_q3.cpp
+++ lab8_q3.cpp
@@ -1,32 +1,38 @@
-//include the libraries
+//include library
 #include<iostream>
 using namespace std;
 
 // function for arranging the array elements
 void arrange (int a[],int n)
-{	int flag,t,i;
+{int flag,l,i;
  	
-	//loop for arranging the array elements in ascending order
+//loop 
  	do
- 	{	flag=0;
-		for(i=0;i<(n-1);i++)
-   		{	if (a[i]>a[i+1])
-    			{ t=a[i];
-     			  a[i]=a[i+1];
-    			  a[i+1]=t;
-     			  flag+=1;
-			}
-		}
-	}while(flag!=0);
+ {	flag=0;
+	for(i=0;i<(n-1);i++)
+		
+   {
+	if (a[i]>a[i+1])
+   	{ l=a[i];
+     	  a[i]=a[i+1];
+    	  a[i+1]=l;
+     	  flag+=1;
+	}
+   }
+	}
+ while(flag!=0);
       
 }
 
-// functions printing kth maximum and kth minimum element of array
+// functions printing kth maximum and kth minimum elements
+
 void klarge(int a[],int n,int k)
-{ cout<<"\n The "<< k<<"th largest element is "<<a[n-k];
+  {
+	cout<<" The "<< k<<"th largest element is "<<a[n-k] << endl;
    }
 void ksmall(int a[],int n,int k)
-{ cout<<"\n The "<< k<<"th smallest element is "<<a[k-1];
+  {
+	cout<<" The "<< k<<"th smallest element is "<<a[k-1] <<endl;
    }
 
 //main function 
@@ -34,23 +40,26 @@ int main()
 {	int a[10],n,k;
  	
 	//ask user for the limit
-	cout<<"\n Enter number of elements in the array ";
+	cout<<" Enter number of elements in the array "<<endl;
  	cin>>n;
  	
-	//ask user for array elements 
-	cout<<"\n Enter array elements ";
+	//array elements 
+	cout<<" Enter array elements "<<endl;
  	for(int i=0;i<n;i++)
   	cin>>a[i];
  	
-	//calling the function to arrange elements in order
+	//calling the function 
 	arrange (a,n);
 	
 	//asks user for the value of k
-	cout<<"\n value of k is ";cin>>k;
+	cout<<"value of k is "<<endl;
+          cin>>k;
 	
 	//calling functions for k th largest and smallest elements
 	klarge(a,n,k);
 	ksmall(a,n,k);
-	cout<<"\n"; return 0;
+	cout<<" "<<endl; 
+ 
+          return 0;
 }
 //end of program
