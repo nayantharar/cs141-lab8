f76070e5e71fe0352d5ff1298fb22343fba3f9b4
nayantharar <42238330+nayantharar@users.noreply.github.com>
2018-11-15 17:42:35 +0530

    Update lab8_q4.cpp

--- lab8_q4.cpp
+++ lab8_q4.cpp
@@ -1,65 +1,75 @@
-//include the libary
+//include  libary
 #include<iostream>
 using namespace std;
 
-// function for merging the arrays
+// function for merging 
 void merge (int a[],int n,int ar[],int k,int m[])
+	
 {	int i;
-	//loop for merging the arrays 
- 	for(i=0;i<n;i++)
+ 
+//loop for merging the arrays
+ 
+	for(i=0;i<n;i++)
    	     m[i]=a[i];
+ 
     	for(i=n;i<n+k;i++)
-             m[i]=ar[i-n];		
-	cout<<"\n printing the merged array";
+             m[i]=ar[i-n];
+ 
+	cout<<"printing the merged array"<<endl;
 	for(i=0;i<n+k;i++)
-	cout<<" "<<m[i];
+	cout<<" "<<m[i]<<endl;
 }
 
 //function for finding the maximum and minimum of the two arrays
 void arrange (int a[],int n)
 {	int flag,t;
  	
-	//loop for arranging the array elements in ascending order
+//loop for arranging the array elements in ascending order
  	do
  	{	flag=0;
 		for(int i=0;i<(n-1);i++)
    		{	if (a[i]>a[i+1])
-    			{ t=a[i];
-     			  a[i]=a[i+1];
-    			  a[i+1]=t;flag++;
+    		{ t=a[i];
+     		  a[i]=a[i+1];
+    		  a[i+1]=t;
+		 flag++;
 			}
 		}
-	}while(flag!=0);
-        cout<<"\n Maximum of the elements in both the array is "<<a[n-1];
-	cout<<"\n Minimum of the elements in both the array is "<<a[0];
+}
+ while(flag!=0);
+cout<<"Maximum of the elements in both the array is "<<a[n-1]<<endl;
+cout<<" Minimum of the elements in both the array is "<<a[0]<<endl;
 }
 
 //main function 
 int main()
-{	int a[15],ar[15],m[30],n,k;
+{	int a[20],ar[20],m[40],n,k;
  	
 	//ask user for the limit of first array
-	cout<<"\n Enter number of elements in the 1st array ";
+	cout<<" Enter number of elements in the 1st array "<<endl;
  	cin>>n;
  	
 	//ask user for array elements 
-	cout<<"\n Enter array elements for first array";
+	cout<<"Enter array elements for first array"<<endl;
  	for(int i=0;i<n;i++)
   	cin>>a[i];
  	
 	//ask user for the limit of seconded array
-	cout<<"\n Enter number of elements in the 2nd array ";
+	cout<<" Enter number of elements in the 2nd array "<<endl;
  	cin>>k;
  	
 	//ask user for array elements 
-	cout<<"\n Enter array elements for second array";
+	cout<<" Enter array elements for second array"<<endl;
  	for(int i=0;i<k;i++)
   	cin>>ar[i];
 	
-	//calling the function to merge arrays
+	//calling the function 
 	merge (a,n,ar,k,m);
+ 
 	//calling function to find minimum of maximum of the two arrays
 	arrange(m,(n+k));
-	cout<<"\n"; return 0;
+	cout<<" "<<endl;
+ return 0;
 }
-//end of program
+
+
